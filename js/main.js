c291f6e97d4ba9f87c8c168d67fae792bdffe68e
David Delgado <davidet123@gmail.com>
2022-09-20 13:02:28 +0200

    Segunda parte vanilla JavaScript ejercicio 1

--- js/main.js
+++ js/main.js
@@ -46,31 +46,82 @@ for (let i = 1; i <= intentosTotales; i++) {
 
 //13
 
-let valor1 = parseInt(prompt("Primer número"));
+/* let valor1 = parseInt(prompt("Primer número"));
 let valor2 = parseInt(prompt("Segundo número"));
 
 let operacion = prompt("Operación")
 
 switch (operacion) {
   case "+":
-    console.log(valor1 + valor2);
+    alert(valor1 + valor2);
     break; 
   case "-":
-    console.log(valor1 - valor2);
+    alert(valor1 - valor2);
     break; 
   case "*":
-    console.log(valor1 * valor2);
+    alert(valor1 * valor2);
     break; 
   case "/":
-    console.log(valor1 / valor2);
+    alert(valor1 / valor2);
     break; 
   case "^":
-    console.log(valor1 ** valor2);
+    alert(valor1 ** valor2);
     break; 
   case "%":
-    console.log(valor1 % valor2);
+    alert(valor1 % valor2);
     break; 
+} */
+
+/* 
+Crea una aplicación que nos calcule el área de un círculo, cuadrado o triangulo.
+Pediremos al usuario que figura queremos calcular su área y según lo introducido
+pedirá los valores necesarios para calcular el área. Para ello has de crear un método
+por cada figura para calcular cada área, este devolverá un número real. Muestra el
+resultado por consola.
+Aquí te mostramos que necesita cada figura:
+• Circulo: (radio^2)*PI
+• Triangulo: (base * altura) / 2
+• Cuadrado: lado * lado
+ */
 
 
+let figura = prompt("Dime una figura").toLowerCase();
+
+
+const CalculaCirculo = () => {
+  let radio = parseInt(prompt("Dime el Radio"))
+
+  return (radio ** 2) * Math.PI
+}
+
+
+const CalculaCuadrado = () => {
+  let lado = parseInt(prompt("Dime el lado"))
+
+  return (lado ** 2);
+}
+
+
+const CalculaTriangulo = () => {
+  let base = parseInt(prompt("Dime la base"))
+  let altura = parseInt(prompt("Dime la altura"))
+
+  return base * altura / 2
+}
+
+
+switch (figura) {
+  case "circulo":
+    console.log(`Área del círculo => ${CalculaCirculo()}`)
+    break;
+  case "triangulo":
+    console.log(`Área del triángulo => ${CalculaTriangulo()}`);
+    break;
+  case "cuadrado":
+    console.log(`Área del cuadrado => ${CalculaCuadrado()}`);
+    break;
+  default:
+    console.log("Figura equivocada")
+    break;
 
 }
